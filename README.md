a77dd6c88f83f9651f9dce842486751101d8ccd4
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2022-02-05 20:53:24 +0000

    remove /api prefix from both paths

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Dialect Server
 
-### v0.3.2
+### v0.3.3
--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	postHandler "dialect-server/api/post"
-	voteHandler "dialect-server/api/vote"
+	postHandler "dialect-server/post"
+	voteHandler "dialect-server/vote"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/api/post", postHandler.Handler)
-	http.HandleFunc("/api/vote", voteHandler.Handler)
+	http.HandleFunc("/post", postHandler.Handler)
+	http.HandleFunc("/vote", voteHandler.Handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
